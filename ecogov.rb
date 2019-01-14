effb43d4ee5724355c7eca533f951bbe3bccb9c5
Fish-bowl <symbolt.ap@gmail.com>
2019-01-14 13:32:14 -0700

    commented all lines

--- ecogov.rb
+++ ecogov.rb
@@ -1,28 +1,36 @@
 
-
+#requiring gems
 require 'pry'
 require 'mechanize'
 require 'rubygems'
-
+#setting search terms
 numbers_value = "541620 326191 562991 423850"
 narrow_value = "bathroom toilet outhouse portapotty restroom"
-
+#creating new instance of mechanize 
 agent = Mechanize.new
 agent.user_agent_alias = "Mac Firefox"
-
+#setting page
 page = agent.get('https://www.fbo.gov/?s=opportunity&mode=list&tab=list')
 
-select.value = select_list.option_with(:value => /1|2/).each do |field|
-  field_value = '100'
-end 
+
+# select_list.option_with(:value => "^0-9.").each do |field|
+#   field_value = '100'
+# end 
 binding.pry
+#setting form for the search terms 
 form = page.form("search_filters")
+#setting values
 main_form = form.fields.first.value=narrow_value
+#setting submit button
 button = form.button_with(:value => "search")
+#submitting form 
 agent.submit(form, button)
-page.links.each do |link|
-  puts link.text
-end 
+
+
+
+# page.links.each do |link|
+#   puts link.text
+# end 
 # binding.pry
 
 # page.forms.each do |form|
